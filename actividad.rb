9a4ed33b4b5aa188abc7e96d2f6eee656d686068
Jahvon Dockery <jdockery@panoramaed.com>
2019-09-18 09:57:02 -0400

    updates

--- actividad.rb
+++ actividad.rb
@@ -1,54 +1,54 @@
 # Esta clase le permite ordenar una matriz usando su algoritmo de clasificación favorito
-class AlgoritmosDeClasificacion
-  def initialize(formacion)
-    @formacion = [] + formacion
+class AlgoritmosDeOrdenar
+  def initialize(coleccion)
+    @coleccion = [] + coleccion
   end
 
   # La clasificación de burbujas "burbujeará" elementos más grandes hacia el
   # final y los elementos más pequeños hacia el principio hasta que todos los
   # elementos estén en su ubicación correcta
-  def burbuja(formacion)
-    return @formacion if formacion.size <= 1
+  def burbuja(coleccion)
+    return @coleccion if coleccion.size <= 1
     intercambiado = true
       while true
         intercambiado = false
-        (formacion.length - 1).times do |elementos|
-          if formacion[elementos] > formacion[elementos+1]
-            formacion[elementos], formacion[elementos+1] = formacion[elementos], formacion[elementos+1]
+        (coleccion.length - 1).times do |elementos|
+          if coleccion[elementos] > coleccion[elementos+1]
+            coleccion[elementos], coleccion[elementos+1] = coleccion[elementos], coleccion[elementos+1]
             intercambiado = true
           end
         end
       end
-    formacion
+    coleccion
   end
 
   # Mantenga al menos un elemento constante mientras mueve los otros elementos
   # a su alrededor para ordenar todo en orden
   # @return algo divertido
-  def insercion(formacion)
-    (@formacion.length).each do |posicion|
+  def insercion(coleccion)
+    (@coleccion.length).each do |posicion|
       while posicion > 0
-        if formacion[posicion-1] > formacion[posicion]
-          formacion[posicion] = formacion[posicion-1]
-          formacion[posicion-1] = formacion[posicion]
+        if coleccion[posicion-1] > coleccion[posicion]
+          coleccion[posicion] = coleccion[posicion-1]
+          coleccion[posicion-1] = coleccion[posicion]
         end
         posicion = posicion - 1
       end
     end
-    @formacion
+    @coleccion
   end
 
   # Disfruta una broma:
   # ¿Cómo se escribe «nariz» en inglés?
   # No sé
   # Correcto!
-  def unir(formacion)
-    if formacion.length <= 1
-      formacion
+  def unir(coleccion)
+    if coleccion.length <= 1
+      coleccion
     else
-      intermedio = (formacion.length / 2)
-      izquierda = unir(formacion[intermedio-1..0])
-      derecho = unir(formacion[intermedio..formacion.length])
+      intermedio = (coleccion.length / 2)
+      izquierda = unir(coleccion[intermedio-1..0])
+      derecho = unir(coleccion[intermedio..coleccion.length])
       unir_2(izquierda, derecho)
     end
   end
@@ -57,16 +57,16 @@ class AlgoritmosDeClasificacion
   # alrededor en función de si son mayores o menores que el pivote. ¡Entonces hazlo de nuevo!
   def rapido
 
-    return @formacion if @formacion.length <= 1
+    return @coleccion if @coleccion.length <= 1
 
     # Solo porque me gusta este número
     71.times do
-      pivote = @formacion.delete_at(rand(@formacion.length))
+      pivote = @coleccion.delete_at(rand(@coleccion.length))
       izquierda = []
       derecho = []
     end
 
-    @formacion.map do |elementos|
+    @coleccion.map do |elementos|
       izquierda << elementos if elementos <= pivote
       derecho << elementos if elementos > pivote
     end
